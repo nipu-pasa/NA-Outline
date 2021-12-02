940753935b2ce32d9bc1b763cf2bd607ab9e0b95
Nipesh Maharjan <88616145+nipu-pasa@users.noreply.github.com>
2021-12-02 21:47:57 +0530

    Read the Description about my Project

---
+++ src/AirlineManagementSystem/Main.java
@@ -0,0 +1,19 @@
+package AirlineManagementSystem;
+
+public class Main {
+
+    public static void main(String[] args) {}
+      /*  IT Projects > Java Projects List >
+                                            Airlines Reservation System
+        The web based “Airline Reservation System” project is an attempt to stimulate the basic concepts of airline reservation system. The system enables the customer to do the things such as search for airline flights for two travel cities on a specified date, choose a flight based on the details, reservation of flight and cancellation of reservation.
+        The system allows the airline passenger to search for flights that are available between the two travel cities, namely the “Departure city” and “Arrival city” for a particular departure and arrival dates. The system displays all the flight’s details such as flight no, name, price and duration of journey etc.
+        After search the system display list of available flights and allows customer to choose a particular flight. Then the system checks for the availability of seats on the flight. If the seats are available then the system allows the passenger to book a seat. Otherwise it asks the user to choose another flight.
+        To book a flight the system asks the customer to enter his details such as name, address, city, state, credit card number and contact number. Then it checks the validity of card and book the flight and update the airline database and user database. The system also allows the customer to cancel his/her reservation, if any problem occurs.
+
+        The main purpose of this software is to reduce the manual errors involved in the airline reservation process and make it convenient for the customers to book the flights as when they require such that they can utilize this software to make reservations, modify reservations or cancel a particular reservation.
+
+        SCOPE
+        The name of the software is “AIRLINE RESERVATION SYSTEM”.
+        This software provides options for viewing different flights available with different timings for a particular date and provides customers with the facility to book a ticket, modify or cancel a particular reservation but it does not provide the customers with details of cost of the ticket and it does not allow the customer to modify a particular part of his reservation and he/she can modify all his details.
+    }
+*/}
